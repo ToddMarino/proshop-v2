ec5f9f7dc9ffed47f5d9145c9c2bfff7843404b4
ToddMarino <todd.j.marino@gmail.com>
2023-10-08 15:35:33 -0500

    product, user, order models

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import products from './data/products.js';
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config()
+import connectDB from './config/db.js';
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
