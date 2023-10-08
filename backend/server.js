a28dd7c4417e0d0faa2d46fac50b06179a88f3d0
ToddMarino <todd.j.marino@gmail.com>
2023-10-08 14:17:10 -0500

    dev, client, server scripts and dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import products from './data/products.js';
 import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
