b97d1fc32e15dda0d7fd91524b8e7095be14b34f
ToddMarino <todd.j.marino@gmail.com>
2023-10-09 16:56:12 -0500

    User Profile endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -11,6 +12,15 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+// Body Parser Middleware
+// these allow our server to get body data with x-www-form-urlencoded in postman
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie Parser middleware
+// allows access to req.cookies.jwt
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running ...');
